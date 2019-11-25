0e1eaf62ebb462cae632e370e602c66538905241
adpietrz <57531384+adpietrz@users.noreply.github.com>
2019-11-25 18:16:16 +0100

    Completed module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -110,9 +110,7 @@ namespace GradeBook.GradeBooks
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {   
             var gpa = 0;
-            if(IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled )){
-                gpa++;
-            }
+            
             
             switch (letterGrade)
             {
@@ -131,6 +129,10 @@ namespace GradeBook.GradeBooks
                 case 'F':
                     gpa = 0;
                     break;
+            }
+            
+            if(IsWeighted = true && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled )){
+                gpa++;
             }
             return gpa;
         }
